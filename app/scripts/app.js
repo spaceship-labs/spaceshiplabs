a25ab5cf60dbd8c091f70d8adb234ab21dc00b43
Luis <luis19prz@gmail.com>
2015-08-10 12:44:21 -0500

    landing avances

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ngMaterial'
+    'ngMaterial',
+    'angular-carousel'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,10 +8,21 @@
  * Controller of the spaceshiplabsApp
  */
 angular.module('spaceshiplabsApp')
-  .controller('HomeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HomeCtrl', function ($scope) {
+
+		$scope.bannerGraphImgs = [0,1];
+	  $scope.bannerGraphImgsIndex = 0;    
+
+	  $scope.carouselNext = function(){
+	    $scope.bannerGraphImgsIndex = ++$scope.bannerGraphImgsIndex % $scope.bannerGraphImgs.length;
+	  };
+
+	  $scope.carouselBack = function(){
+	    $scope.bannerGraphImgsIndex = --$scope.bannerGraphImgsIndex % $scope.bannerGraphImgs.length;
+	  };
+
+	  $scope.goToSlide = function(index){
+	    $scope.bannerGraphImgsIndex = index;
+	  }
+
   });
