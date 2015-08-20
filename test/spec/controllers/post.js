03df6523d88bfab5338ee01f708c6b1d3d4e3a39
Luis <luis19prz@gmail.com>
2015-08-20 13:20:20 -0500

    fix test

--- test/spec/controllers/post.js
+++ test/spec/controllers/post.js
@@ -17,7 +17,4 @@ describe('Controller: PostCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(PostCtrl.awesomeThings.length).toBe(3);
-  });
 });
