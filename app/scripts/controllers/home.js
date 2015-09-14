b3807806023f3b82406ee59cfa0b11c3461d429d
Luis <luis19prz@gmail.com>
2015-09-14 17:53:02 -0500

    fixed header y projects slider

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -29,6 +29,23 @@ angular.module('spaceshiplabsApp')
       );
     };
 
+    $scope.projects = [
+      {
+        type:'UI/UX Design & Software',
+        name:'Yellow Transfers',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem accusamus fugit, saepe, earum, illum esse sed obcaecati molestiae ducimus illo omnis! Dolores reiciendis sed non iure dolore tempore sit facere!',
+        url: '#',
+        images:['images/yellow_proyectos.jpg','images/lap.png']
+      },
+      {
+        type:'UI/UX Design',
+        name:'Capshia',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem accusamus fugit, saepe, earum, illum esse sed obcaecati molestiae ducimus illo omnis! Dolores reiciendis sed non iure dolore tempore sit facere!',
+        url: '#',
+        images: ['images/projects/capshia.jpg','images/projects/capshia2.jpg']
+      }
+    ];
+
     $scope.getRecentPosts();
 
   });
