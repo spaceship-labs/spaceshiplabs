67846a97501369d8291f8501b49718ba91406918
Luis <luis19prz@gmail.com>
2015-09-14 11:12:24 -0500

    contact service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,10 @@
  * Controller of the spaceshiplabsApp
  */
 angular.module('spaceshiplabsApp')
-  .controller('MainCtrl', function ($scope, $mdSidenav, $location, metaTagsService) {
+  .controller('MainCtrl', function ($scope, $mdSidenav, $location, metaTagsService, $http) {
+    $scope.emailSent = false;
+    $scope.contactError = false;
+    $scope.contactData = {};
 
 	  $scope.toggleSidebar = function() {
       $mdSidenav('left').open();
@@ -23,6 +26,37 @@ angular.module('spaceshiplabsApp')
     $scope.getAbsUrl = function(){
       return $location.absUrl();
     };
+
+    $scope.doContact = function(){
+      $scope.contactError = false;
+      $scope.emailSent = false;
+      var req = {
+        method: 'POST',
+        url: '/__/forms/contact',
+        //method: 'GET',
+        //url: '/json/email-success.json',
+        data: {
+          email: $scope.contactData.email,
+          name: $scope.contactData.name,
+          message: $scope.contactData.message
+        },
+      };
+      $http( req ).then(
+        function(response) {
+          console.log(response);
+          if(response.data.status === 'success'){
+            $scope.emailSent = true;
+          }else{
+            $scope.contactError = true;
+          }
+        }, function(err) {
+          $scope.contactError = true;
+          console.log(err);
+        }
+      );
+
+    };
+
 
     metaTagsService.setMetaTags();
 
