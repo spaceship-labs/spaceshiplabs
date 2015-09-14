3d301318fa811eb97cc09f4b054e50bf4e35bb26
Luis <luis19prz@gmail.com>
2015-09-14 13:36:43 -0500

    contact single page

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -9,4 +9,27 @@
  */
 angular.module('spaceshiplabsApp')
   .controller('ContactCtrl', function () {
+
+    var initialize = function() {
+
+      var mapOptions = {
+       center: { lat: 21.1656951, lng: -86.8210734},
+       zoom: 18,
+       //scrollwheel: false
+      };
+      var map = new google.maps.Map(document.getElementById('map-contact'),
+         mapOptions);
+      var locationLatLng = new google.maps.LatLng(21.1656951,-86.8210734);
+
+      var marker = new google.maps.Marker({
+          position: locationLatLng,
+          title:"Spaceship Labs"
+      });
+
+      // To add the marker to the map, call setMap();
+      marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
   });
