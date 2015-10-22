0b3565825ebce1dff63631e53ef66c9c85bd44a4
Luis <luis19prz@gmail.com>
2015-10-22 17:22:48 -0500

    uiux icons

--- app/scripts/controllers/ui-ux.js
+++ app/scripts/controllers/ui-ux.js
@@ -7,14 +7,42 @@
  * # UiUxCtrl
  * Controller of the spaceshiplabsApp
  */
-function UiUxCtrl($scope,metaTagsService){
+function UiUxCtrl($scope, $interval,metaTagsService){
   $scope.init = function(){
     var meta = {
       title: 'UI/UX | SpaceshipLabs'
     };
     metaTagsService.setMetaTags(meta);
+    $scope.designSliderInterval = $interval($scope.moveNextDesignSlider, 5000);
   };
+
+  $scope.designSliderIndex = 0;
+  $scope.logos = [
+    'images/open_data.png',
+    'images/slack.png'
+  ];
+  $scope.moveDesignSlider = function(index){
+    $scope.designSliderIndex = index;
+    $interval.cancel( $scope.designSliderInterval );
+  }
+  $scope.moveNextDesignSlider = function(){
+    if($scope.designSliderIndex < $scope.logos.length-1){
+      $scope.designSliderIndex++;
+    }else{
+      $scope.designSliderIndex  = 0;
+    }
+  }
+
+  $scope.$on(
+      "$destroy",
+      function() {
+          $interval.cancel( scope.designSliderInterval );
+      }
+  );
+
   $scope.init();
+
+
 }
-UiUxCtrl.$inject = ['$scope','metaTagsService'];
+UiUxCtrl.$inject = ['$scope','$interval','metaTagsService'];
 angular.module('spaceshiplabsApp').controller('UiUxCtrl',UiUxCtrl);
