fe8414cfbe382ff36335420dd36682f8a8b3bad1
Gibrán de Jesús Ramón Perera <gibranjramon@gmail.com>
2021-05-03 20:09:24 -0500

    Minor fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -284,7 +284,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: '{,*/}*.{png,jpg,jpeg,gif,webp}',
           dest: '<%= yeoman.dist %>/images'
         }]
       }
